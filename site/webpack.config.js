d45598fee23f2adb34baf6e1ffb680ca54db40c1
PrettyWood <em.jolibois@gmail.com>
2020-12-27 23:01:35 +0100

    build: use TS instead of JS

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./bootstrap.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   mode: "development"
 };
